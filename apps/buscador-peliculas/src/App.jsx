3cc758aa343091ea85d18fd8bc1e1756718cddf5
dsaza <dayrondaza05@gmail.com>
2023-03-15 17:41:20 -0500

    Update folders

---
+++ apps/buscador-peliculas/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <form>
+        <input type="text" placeholder="Avengers, Star Wars, The Matrix..." />
+        <button type="submit">Buscar</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
